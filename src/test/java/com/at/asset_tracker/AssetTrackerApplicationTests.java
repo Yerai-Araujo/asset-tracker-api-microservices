c473839ec2d5e2467f3fbe76353854c086a09c02
Yerai-Araujo <yeraiaraujo227@gmail.com>
2026-02-05 16:53:53 +0100

    Add Spring Boot project

---
+++ src/test/java/com/at/asset_tracker/AssetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.at.asset_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
